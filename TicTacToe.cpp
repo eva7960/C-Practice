d870a288e2dd3d127878d3e2cf2b40b6d0f4af4c
Eva Howard <7960.eva@gmail.com>
2025-01-16 01:00:44 -0800

    Update TicTacToe.cpp
    Game logic works but I want to make it replayable.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -8,28 +8,43 @@
  * @version 1.0 1/7/25
  */
 //function signatures
+void drawGuide();
 void drawBoard(char *spaces);
-void playerMove(char *spaces, char player);
-void computerMove(char *spaces, char computer);
-bool checkWinner(char *spaces, char player, char computer);
+void playerMove(char *spaces);
+void computerMove(char *spaces);
+bool checkWinner(char *spaces);
 bool checkTie(char *spaces);
 
 int main() {
     
     char spaces[9] = { ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' '};
-    char player = 'X';
-    char computer = 'O';
     bool running = true;
 
-    drawBoard(spaces);
+    drawGuide();
 
     while(running) {
-        playerMove(spaces, player);
+        playerMove(spaces);
         drawBoard(spaces);
-        computerMove(spaces, computer);
+        running = !checkWinner(spaces);
+        std::cout << "\n\nComputer's Turn:\n";
+        computerMove(spaces);
         drawBoard(spaces);
+        running = !checkWinner(spaces);
+        
     }
+    std::cout << "\nThanks for playing!";
     return 0;
+}
+void drawGuide() {
+    std::cout << "     |     |     " << '\n'; //print statement and new line
+    std::cout << "  1  |  2  |  3  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n'; 
+    std::cout << "  4  |  5  |  6  " << '\n';
+    std::cout << "_____|_____|_____" << '\n';
+    std::cout << "     |     |     " << '\n';
+    std::cout << "  7  |  8  |  9  " << '\n';
+    std::cout << "     |     |     " << '\n';
 }
 
 void drawBoard(char *spaces) {
@@ -54,9 +69,8 @@ void playerMove(char *spaces) {
             break;
         }
     } while (!number > 0 || !number < 8);
-
-}
-void computerMove(char *spaces) {
+    
+}void computerMove(char *spaces) {
     int number;
     srand(time(0)); // generate random number
     while(true) {
@@ -68,15 +82,38 @@ void computerMove(char *spaces) {
     }
 
 }
-bool checkWinner(char *spaces, char player, char computer) {
-    if(spaces[0] == spaces[1] && spaces[1] == spaces[2]) {
-
+bool checkWinner(char *spaces) {
+    if(spaces[0] != ' ' && spaces[0] == spaces[1] && spaces[0] == spaces[2]) {
+        spaces[0] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; //if else statement
+    } else if(spaces[3] != ' ' && spaces[3] == spaces[4] && spaces[3] == spaces[5]) {
+        spaces[3] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(spaces[6] != ' ' && spaces[6] == spaces[7] && spaces[6] == spaces[8]) {
+        spaces[6] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!";
+    } else if(spaces[0] != ' ' && spaces[0] == spaces[3] && spaces[0] == spaces[6]) {
+        spaces[0] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(spaces[1] != ' ' && spaces[1] == spaces[4] && spaces[1] == spaces[7]) {
+        spaces[1] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(spaces[2] != ' ' &&  spaces[2] == spaces[5] && spaces[2] == spaces[8]) {
+        spaces[2] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(spaces[0] != ' ' && spaces[0] == spaces[4] && spaces[4] == spaces[8]) {
+        spaces[0] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(spaces[2] != ' ' && spaces[2] == spaces[4] && spaces[2] == spaces[6]) {
+        spaces[2] == 'X' ? std::cout << "YOU WIN!" : std::cout << "YOU LOSE!"; 
+    } else if(checkTie(spaces)) {
+        std::cout << "IT'S A TIE!";
+        return false;
+    } else {
+        return false;
     }
     return true;
 
 }
 bool checkTie(char *spaces){
-
+    for(int i = 0; i < 9; i++) {
+        if(spaces[i] == ' ') {
+            return false;
+        }
+    }
     return true;
 
 }
